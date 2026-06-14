685f1138e552d391378424fa164857bc63d35357
Wesley Portugal <wesleyportugal250@gmail.com>
2026-06-14 01:43:03 -0300

    Adição dos components shadcn  input, button, card e separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
 const Home = () => {
   return (
-    <div>Wesley</div>
-  )
-}
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg p-4">
+        <div className="flex gap-2">
+          <Input />
+          <Button variant="outline">Cadastrar</Button>
+        </div>
 
-export default Home
+        <Separator />
+
+      </Card>
+    </main>
+  );
+};
+
+export default Home;
