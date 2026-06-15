41ca6dad74ec560c9b081ba6331ee37e97d36c32
Wesley Portugal <wesleyportugal250@gmail.com>
2026-06-14 23:08:36 -0300

    Adição de Shadcn Badge Todas, Não finalizadas e Concluídas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { Plus, List, Check, ArrowDownRight } from "lucide-react";
 
 const Home = () => {
   return (
     <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
       <Card className="w-lg p-4">
         <div className="flex gap-2">
-          <Input />
-          <Button variant="outline">Cadastrar</Button>
+          <Input placeholder="Adicionar tarefa" />
+          <Button variant="outline" className="cursor-pointer">
+            <Plus />
+            Cadastrar
+          </Button>
         </div>
 
         <Separator />
 
+        <div className="flex gap-2">
+          <Badge className="cursor-pointer">
+            <List />
+            Todas
+          </Badge>
+          <Badge className="cursor-pointer">
+            <ArrowDownRight />
+            Não finalizadas
+          </Badge>
+          <Badge className="cursor-pointer">
+            <Check />
+            Concluídas
+          </Badge>
+        </div>
       </Card>
     </main>
   );
